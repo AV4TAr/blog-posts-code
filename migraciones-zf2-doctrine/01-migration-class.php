703d705e79e80623fbc31e28ee20ffcf7e9d842c
Diego Sapriza <AV4TAr@gmail.com>
2013-12-05 10:06:01 -0200

    ini

---
+++ migraciones-zf2-doctrine/01-migration-class.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * 
+ */
+class Version20131204220451 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+    }
+
+    public function down(Schema $schema)
+    {
+    }
+}
